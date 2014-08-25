09c38586813fbad73592e6e7005a5afc42a3dee6
paic <njurice@gmail.com>
2014-08-25 20:50:14 +0800

    merge

--- client/application/src/com/young/module/location/CityManageActivity.java
+++ client/application/src/com/young/module/location/CityManageActivity.java
@@ -1,29 +1,21 @@
 package com.young.module.location;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
 import com.google.gson.Gson;
-import com.young.MyApplication;
-import com.young.common.util.HttpUtil;
+import com.google.gson.reflect.TypeToken;
 import com.young.common.util.L;
 import com.young.common.util.SharePreferenceUtil;
-import com.young.module.weather.GetWeatherTask;
+import com.young.entity.City;
 import com.young.module.weather.MainActivity;
 import com.young.sort.list.DragSortListView;
 import com.young.sort.list.SimpleDragSortCursorAdapter;
 import com.young.modules.R;
-import com.young.modules.R.id;
-import com.young.modules.R.layout;
-import com.young.modules.R.menu;
 
 import android.database.MatrixCursor;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.app.ActionBar;
 import android.content.Context;
@@ -35,18 +27,45 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.database.Cursor;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CityManageActivity extends FragmentActivity {
 
+	public static final int UPDATE_CITY_SCUESS = 1;
+	public static final int UPDATE_CITY_FAIL = 0;
+	
     private SimpleDragSortCursorAdapter adapter;
     private SharePreferenceUtil mSpUtil;
+    private Context mContext;
     
-    private List<String> cityList;
-    private List<String> oCityList;
+    private List<City> cityList;
+    private int removeCityIndex;
+    private int selectCityIndex = 0;
+    
+    private Handler handler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			switch (msg.what) {
+			case UPDATE_CITY_SCUESS:
+				//startManageActivity();
+				cityList.remove(removeCityIndex); 
+				saveCityChangeToSharePreference(cityList);
+				break;
+			case UPDATE_CITY_FAIL:
+				cityList.remove(removeCityIndex); 
+				saveCityChangeToSharePreference(cityList);
+				Toast.makeText(mContext, "update fail", Toast.LENGTH_SHORT).show();
+				break;
+			default:
+				break;
+			}
+		}
+	};
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this;
         setContentView(R.layout.city_manage);
         
         if (mSpUtil == null)
@@ -60,7 +79,6 @@ public class CityManageActivity extends FragmentActivity {
         DragSortListView dslv = (DragSortListView) findViewById(android.R.id.list);
         dslv.setAdapter(adapter);
         
-        oCityList = loadAllCityFromSharePreference();
         cityList = loadAllCityFromSharePreference();
         loadCity(cityList);
         
@@ -68,27 +86,25 @@ public class CityManageActivity extends FragmentActivity {
         bar.setDisplayHomeAsUpEnabled(true);
     }
     
-	public List<String> loadAllCityFromSharePreference() {		
-		List<String> citys = new ArrayList<String>();
+    public List<City> loadAllCityFromSharePreference() {		
+		List<City> citys = new ArrayList<City>();
 		try {
-			JSONArray cityList = new JSONArray(mSpUtil.getAllCity().toString());
-			for(int i=0; i<cityList.length();i++){
-				String city = (String) cityList.get(i);
-				citys.add(city);
-			}
-		} catch (JSONException e) {
+			Gson gson = new Gson();
+			Type lt=new TypeToken<List<City>>(){}.getType();
+			citys=gson.fromJson(mSpUtil.getAllCity().toString(),lt);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return citys;
 	}
 
-	private void loadCity(List<String> cityList){
+	private void loadCity(List<City> cityList){
     	MatrixCursor cursor = new MatrixCursor(new String[] {"_id", "name"});        
         if(null != cityList && cityList.size() > 0){
 	    	for (int i = 0; i < cityList.size(); i++) {
 	            cursor.newRow()
 	                    .add(i)
-	                    .add(cityList.get(i));
+	                    .add(cityList.get(i).getName());
 	        }
         }else{
         	cursor.newRow().add(0).add("请添加城市");
@@ -105,15 +121,13 @@ public class CityManageActivity extends FragmentActivity {
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             View v = super.getView(position, convertView, parent);
             TextView tv = (TextView) v.findViewById(R.id.text);
             tv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                	String cityName = (String) ((TextView) v).getText();
-                    int cityIndex = cityList.indexOf(cityName);
-                    startMainActivity(cityIndex);
+                    startMainActivity(position);
                 }
             });
             return v;
@@ -123,8 +137,8 @@ public class CityManageActivity extends FragmentActivity {
         public void doAfterDrop(int from, int to) {
         	//do something after drop
         	L.i("drop from "+ from +" to "+to);
-        	String cityFrom = cityList.get(from);
-        	String cityTo = cityList.get(to);
+        	City cityFrom = cityList.get(from);
+        	City cityTo = cityList.get(to);
         	cityList.set(to, cityFrom);
         	cityList.set(from, cityTo);
         	saveCityChangeToSharePreference(cityList);
@@ -134,8 +148,8 @@ public class CityManageActivity extends FragmentActivity {
         public void doAfterRemove(int which)  {
         	//do something after remove
         	L.i("remove city " + which);
-        	cityList.remove(which);
-        	saveCityChangeToSharePreference(cityList);
+        	new ChangeMyCitiesTask(handler, cityList.get(which), "ModLocation/delete").execute();
+        	removeCityIndex = which;
         }
     }
     
@@ -152,7 +166,6 @@ public class CityManageActivity extends FragmentActivity {
 		switch (item.getItemId()) {
 		
 		case android.R.id.home:        
-			saveCityChangesToServer();		 
 			Intent intent = new Intent(this, MainActivity.class);            
 			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); 
 			startActivity(intent);       
@@ -174,51 +187,31 @@ public class CityManageActivity extends FragmentActivity {
 	}
     
     private void startMainActivity(int city) {
-		Intent i = new Intent();
+		Intent i = new Intent();		
+		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		i.putExtra("cityIndex", city);
 		setResult(RESULT_OK, i);
-		saveCityChangesToServer();
-		new GetWeatherTask(null, cityList.get(city)).execute();
-		finish();
+		this.finish();
 	}
     
     @Override  
     protected void onActivityResult(int requestCode, int resultCode, Intent data)  
     {  
     	if (requestCode == 0 && resultCode == RESULT_OK) {
-    		String cityname = data.getExtras().getString("city");
-    		cityList.add(cityname);
-			saveCityChangeToSharePreference(cityList);
+//    		String cityname = data.getExtras().getString("city");
+//    		cityList.add(cityname);
+//			saveCityChangeToSharePreference(cityList);
+    		cityList = loadAllCityFromSharePreference();
 			loadCity(cityList);
 		}
     }  
     
-    private void saveCityChangeToSharePreference(List<String> newCitys) {
+    private void saveCityChangeToSharePreference(List<City> newCitys) {
 		// TODO Auto-generated method stub
 		Gson gson = new Gson();
 		String citys = gson.toJson(newCitys);
 		L.i(citys);
-		mSpUtil.setAllCity(citys);		
+		MainActivity.mSpUtil.setAllCity(citys);		
 	}
 
-	private void saveCityChangesToServer() {
-    	if(!oCityList.equals(cityList)){
-			List<String> otherCitys = new ArrayList<String>(cityList);
-			otherCitys.remove(0);
-			Map<String ,Object> rawParams = new HashMap<String, Object>();
-			rawParams.put("stay", cityList.get(0));
-			rawParams.put("other", otherCitys);
-			try {
-				HttpUtil.postRequest("http://106.187.94.192/weather/index.php?r=machine/ChangeLocation", rawParams);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-    }
-    
-//    @Override
-//    public void onDestroy() {
-//    	
-//    }
 }
